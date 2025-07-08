0194a9442e1b211416b881c266cedaa0b1cfd186
Sampath Bageyawadi <sampathbageyawadi1@outlook.com>
2025-07-08 22:18:21 +0530

    app.py

--- app.py
+++ app.py
@@ -36,12 +36,12 @@ col1, col2, col3 = st.columns([1, 1, 1])
 
 with col1:
     if st.button("🎤 Vocal & Non-Vocal Separation", use_container_width=True):
-        st.switch_page("pages/1_Vocal_and_NonVocal_Separation.py")
+        st.switch_page("Pages/1_Vocal_and_NonVocal_Separation.py")
 
 with col2:
     if st.button("🎛️ Vocal processing", use_container_width=True):
-        st.switch_page("pages/2_Vocal_Processing.py")
+        st.switch_page("Pages/2_Vocal_Processing.py")
 
 with col3:
     if st.button("🎶 Generate AI Music", use_container_width=True):
-        st.switch_page("pages/3_Generate_AI_Music.py")
+        st.switch_page("Pages/3_Generate_AI_Music.py")
